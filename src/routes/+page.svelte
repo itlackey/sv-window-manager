72a7e9364a00631704f6cd829cc2dd29e430ed7b
itlackey <itlackey@gmail.com>
2025-10-13 01:00:17 -0500

    feat: Update +page.svelte to include WindowManagerShell component and remove placeholder text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import WindowManagerShell from "$lib/WindowManagerShell.svelte";
+
+</script>
 <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<WindowManagerShell title="Demo Workspace">
+   
+</WindowManagerShell>
