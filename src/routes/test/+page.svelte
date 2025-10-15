7049506d48b6bea35584a9ae875161070b383ff8
itlackey <itlackey@gmail.com>
2025-10-14 21:41:31 -0500

    feat(bwin): update styles and structure for BwinHost component; remove .vscode from .gitignore

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -10,8 +10,6 @@
 	import BwinHost from '$lib/components/bwin/BwinHost.svelte';
 	let bwinHostRef: BwinHost = $state<BwinHost | undefined>();
 
-	
-
 	// Mock API data for sessions (sessionId -> {type, data})
 	const sessionData = {
 		abc123: { type: 'chat', data: { welcome: 'Hello Chat!' } },
@@ -61,7 +59,7 @@
 
 	function refresh() {
 		bwinInfo = bwinHostRef.getInfo();
-        console.log('Refreshed bwinInfo:', bwinInfo);
+		console.log('Refreshed bwinInfo:', bwinInfo);
 	}
 </script>
 
@@ -72,6 +70,7 @@
 <div class="session-opener">
 	<input type="text" bind:value={sessionId} placeholder="Enter Session ID" />
 	<button onclick={openSession}>Open Session</button>
+	<button onclick={refresh}>Refresh</button>
 </div>
 
 <BwinHost
@@ -81,11 +80,50 @@
 	}}
 />
 
-<button onclick={refresh}>Refresh</button>
 {#key bwinInfo}
 	{#if bwinInfo}
-		<pre><code>
+		<pre style="display:none;"><code>
     {JSON.stringify(bwinInfo, null, 2)}
 </code></pre>
 	{/if}
 {/key}
+
+<style>
+	:root {
+		--accent-color: hsla(226, 80%, 27%, 0.6);
+		--accent-color-strong: hsla(226, 80%, 27%, 0.9);
+		--accent-color-light: hsla(226, 50%, 75%, 0.6);
+		--bg-color: hsla(0, 0%, 95%, 1);
+		--bg-color-dark: hsla(0, 0%, 20%, 1);
+		--disabled-color: hsla(0, 0%, 80%, 0.6);
+
+		/* Typography */
+		--bw-font-family: inherit;
+		--bw-font-size: inherit;
+
+		/* Colors */
+		--bw-drop-area-bg-color: var(--accent-color-light);
+		--bw-pane-bg-color: var(--bg-color-dark);
+		--bw-muntin-bg-color: var(--accent-color-strong);
+		--bw-glass-bg-color: var(--bg-color);
+		--bw-glass-border-color: var(--accent-color-strong);
+		--bw-glass-border-color-disabled: var(--disabled-color);
+		--bw-glass-bg-color-disabled: var(--accent-color-light);
+		--bw-glass-header-bg-color: var(--accent-color);
+		--bw-glass-tab-hover-bg: var(--accent-color-light);
+		--bw-glass-action-hover-bg: var(--accent-color-light);
+		--bw-minimized-glass-hover-bg: var(--accent-color-light);
+
+		/* Sizing & Spacing */
+		--bw-container-width: stretch;
+		--bw-container-height: calc(100svh - 50px);
+		--bw-glass-clearance: 2px;
+		--bw-glass-border-radius: 5px;
+		--bw-glass-header-height: 30px;
+		--bw-glass-header-gap: 4px;
+		--bw-sill-gap: 6px;
+		--bw-action-gap: 2px;
+		--bw-minimized-glass-height: 10px;
+		--bw-minimized-glass-basis: 10%;
+	}
+</style>
