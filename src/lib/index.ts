3e3c6164308dbd4b0d5c722273392e6128447b5e
itlackey <itlackey@gmail.com>
2025-10-20 11:09:47 -0500

    removing custom implementation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,16 +1,2 @@
 // Public exports for the SV Window Manager library
 
-export type {
-	ShellConfig,
-	KeyboardConfig,
-	AppearanceConfig,
-	PanelState,
-	ReadyDetail,
-	Tab
-} from './types.js';
-
-export { default as WindowManagerShell } from './WindowManagerShell.svelte';
-export { default as ExamplePanel } from './ExamplePanel.svelte';
-export { default as FlashErrorOverlay } from './components/FlashErrorOverlay.svelte';
-export { default as TabBar } from './components/TabBar.svelte';
-export type { FlashItem } from './types.js';
